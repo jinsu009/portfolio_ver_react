9caa5eedb67a7f2d75c80fcc52c4fc60e7a0daf5
jinsu009 <chlrkd_009@naver.com>
2025-09-25 15:08:43 +0900

    change background color

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,60 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import "./Home.css";
+import { start } from "repl";
+
+const useScrollGradient = (
+  startColor: [number, number, number],
+  endColor: [number, number, number]
+) => {
+  const [currentBgColor, setCurrentBgColor] = useState(
+    `rgb(${startColor.join(",")})`
+  );
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const windowHeight = window.innerHeight;
+      const docHeight = document.documentElement.scrollHeight;
+
+      //스크롤 기능 영역 계산 (전체 문서 높이 - 뷰포트 높이)
+      const maxScroll = docHeight - windowHeight;
+
+      // 스크롤 진행률
+      const scrollProgress =
+        maxScroll > 0 ? Math.min(scrollY / maxScroll, 1) : 0;
+
+      // 스크롤 진행률에 따라 각 색상 채널 값 계산
+      const newColor = startColor.map((start, index) => {
+        const end = endColor[index];
+        return Math.round(start + (end - start) * scrollProgress);
+      });
+
+      setCurrentBgColor(`rgb(${newColor.join(",")})`);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [startColor, endColor]);
+  return currentBgColor;
+};
 
 export const Home = () => {
-  window.addEventListener("scroll", function () {
-    console.log("hi");
-  });
+  const startColor = [91, 173, 255] as [number, number, number];
+  const endColor = [10, 80, 80] as [number, number, number];
 
-  return <div className="home_div">Home</div>;
+  const backgroundColor = useScrollGradient(startColor, endColor);
+  return (
+    <div
+      className="home_div"
+      style={{
+        backgroundColor,
+      }}
+    >
+      Home
+    </div>
+  );
 };
